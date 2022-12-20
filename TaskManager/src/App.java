1b83320ff1e38baed9950182285058d80f6ec577
Asakur <Asakur99@gmail.com>
2022-12-20 14:04:56 +0300

    TaskManager Done
    Насколько я понял - выполненным задание считается, если дата выполнения раньше, чем сегодняшняя.

---
+++ TaskManager/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Добро пожаловать в планировщик заданий!");
+        TaskMgr taskMgr = new TaskMgr();
+
+    }
+}

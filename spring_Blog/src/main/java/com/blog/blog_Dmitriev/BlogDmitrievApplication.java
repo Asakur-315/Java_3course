2c1d3ae7d673f26d42b0e217e0d3f2418bb23d5f
Asakur <40217318+Asakur-315@users.noreply.github.com>
2023-03-31 16:03:55 +0300

    Spring_blog

---
+++ spring_Blog/src/main/java/com/blog/blog_Dmitriev/BlogDmitrievApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blog_Dmitriev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDmitrievApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDmitrievApplication.class, args);
+	}
+
+}
---
+++ spring_Blog/src/test/java/com/blog/blog_Dmitriev/BlogDmitrievApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blog_Dmitriev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDmitrievApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
